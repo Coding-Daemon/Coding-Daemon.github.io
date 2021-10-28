b182fe33f20bf16de3853d62b1a8afc205cca8d3
Coding Daemon <66430301+Coding-Daemon@users.noreply.github.com>
2021-10-28 11:47:44 +0530

    First Upload

---
+++ js/pico_ducky.js
@@ -0,0 +1,20 @@
+
+    function myFunction() {
+        navigator.clipboard.writeText("sudo ssh pi@(ip address of Raspberry Pi)");
+      }
+  
+  
+      function myFunction2() {
+   
+        navigator.clipboard.writeText("sudo apt update && sudo apt upgrade");
+      }
+  
+  
+      function myFunction3() {
+      navigator.clipboard.writeText("sudo wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash");
+      }
+  
+  
+      function myFunction4() {
+        navigator.clipboard.writeText("nfs://(IP Address)/export/(Name Of Shared Folder)");
+      }
